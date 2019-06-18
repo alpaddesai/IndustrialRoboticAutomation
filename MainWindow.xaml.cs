73a57feb7dffdfa67101e6ffe16a679dee6de2f2
unknown <alpa.gupta@gmail.com>
2019-06-18 17:35:00 -0400

    initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IndustrialAutomationRobotics
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Lean_Manufacturing_Click(object sender, RoutedEventArgs e)
+        {
+            LeanManufacturing LeanManufacturingObject = new LeanManufacturing();
+            LeanManufacturingObject.Show();
+
+        }
+
+        private void SenorsRobotics_Click(object sender, RoutedEventArgs e)
+        {
+            SensorsRobotics SensorsRoboticsObject = new SensorsRobotics();
+            SensorsRoboticsObject.Show();
+        }
+    }
+}
---
+++ SensorsRobotics.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IndustrialAutomationRobotics
+{
+    /// <summary>
+    /// Interaction logic for SensorsRobotics.xaml
+    /// </summary>
+    public partial class SensorsRobotics : Window
+    {
+        public SensorsRobotics()
+        {
+            InitializeComponent();
+        }
+
+        private void RichTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
